ef0f8cacc62fcb44cc163e0ce8187321eac8586d
Sebastian Hüftlein <sebastian.hueftlein@gmail.com>
2021-03-25 21:40:25 +0100

    rename

--- sendmessage/app.js
+++ sendmessage/app.js
@@ -5,13 +5,13 @@ const AWS = require('aws-sdk');
 
 const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
 
-const { TABLE_NAME } = process.env;
+const { CONNECTION_TABLE_NAME } = process.env;
 
 exports.handler = async event => {
   let connectionData;
   
   try {
-    connectionData = await ddb.scan({ TableName: TABLE_NAME, ProjectionExpression: 'connectionId' }).promise();
+    connectionData = await ddb.scan({ TableName: CONNECTION_TABLE_NAME, ProjectionExpression: 'connectionId' }).promise();
   } catch (e) {
     return { statusCode: 500, body: e.stack };
   }
@@ -29,7 +29,7 @@ exports.handler = async event => {
     } catch (e) {
       if (e.statusCode === 410) {
         console.log(`Found stale connection, deleting ${connectionId}`);
-        await ddb.delete({ TableName: TABLE_NAME, Key: { connectionId } }).promise();
+        await ddb.delete({ TableName: CONNECTION_TABLE_NAME, Key: { connectionId } }).promise();
       } else {
         throw e;
       }
